c54a75240f66cde6ead7c491cf68360d68579785
Yagyashish <87017001+Yagyashish@users.noreply.github.com>
2021-07-08 13:23:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ function compute()
 {
     p = If you deposit "document.getElementById("principal").value" <br>
         at an interest rate of "document.getElementById("rate").value" <br>
-        you will receive an amount of 
+        you will receive an amount of "document.getElementById("result").value" <br>
+        in the year
     
 }
         
